63f4fe3d49373fee90e3e380cc3096e22c6c4af7
Mario Goodwin <mr.goodwin@outlook.com>
2018-07-12 12:30:23 -0400

    Switch to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180711015605) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "maps", force: :cascade do |t|
     t.string "image_name"
     t.datetime "created_at", null: false
